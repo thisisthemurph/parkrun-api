171d77cf06e00badfff529373d4cac2ac53f6773
= <michael.w.murphy@outlook.com>
2020-03-31 14:04:25 +0100

    Created a simple server to serve the JSON results

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
-const processUser = require('./parser')
+const express = require('express')
+const processUser = require('./scraper')
 
-processUser(146388)
+const app = express()
+app.use(express.json())
+
+app.get('/parkrun/api', (req, res) => res.json({'message': 'Hello Parkrun!'}))
+
+// const testUsers = {
+//   user1: 5997634,
+//   user2: 146388,
+//   user3: 717551,
+//   user4: 6106724  // has duration over 1 hour
+// }
+
+app.get('/parkrun/api/:userId', async (req, res) => {
+    const user = await processUser(req.params.userId)
+    res.json(user)
+})
+
+const port = 3000
+app.listen(port, () => console.log(`Listening on port ${port}`))
