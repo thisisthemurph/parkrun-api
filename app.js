13733ac158436a134c489f7edd69877ddec83a61
= <michael.w.murphy@outlook.com>
2020-04-10 20:27:01 +0100

    Removed test code

--- app.js
+++ app.js
@@ -6,14 +6,9 @@ app.use(express.json())
 
 app.get('/parkrun/api', (req, res) => res.json({'message': 'Hello Parkrun!'}))
 
-// const testUsers = {
-//   user1: 5997634,
-//   user2: 146388,
-//   user3: 717551,
-//   user4: 6106724  // has duration over 1 hour
-// }
-
 app.get('/parkrun/api/:userId', async (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+
   if (await userExists(req.params.userId)) {
     const user = await processUser(req.params.userId)
     user.name = await scrapeUserName(req.params.userId)
@@ -25,5 +20,5 @@ app.get('/parkrun/api/:userId', async (req, res) => {
   }
 })
 
-const port = 3000
+const port = 5000
 app.listen(port, () => console.log(`Listening on port ${port}`))
